60be7f8189735a8da37a9d2cafb0b70cf7929527
vlkingiii <vernonlee.kingiii@gmail.com>
2026-04-15 21:32:10 -0700

    add style and theme classification with clean output

--- classifier.py
+++ classifier.py
@@ -5,8 +5,16 @@ classifier = pipeline("zero-shot-image-classification", model="openai/clip-vit-b
 
 image = Image.open("test_art.jpg")
 
-labels = ["watercolor painting","gouache","fresco", "oil painting","acrylic painting", "watercolor", "pencil sketch", "digital art", "charcoal drawing"]
+medium_labels = ["watercolor painting","gouache","fresco", "oil painting","acrylic painting", "watercolor", "pencil sketch", "digital art", "charcoal drawing"]
+style_labels = ["cubism", "neo_mannerism", "impressionism", "expressionism"]
+theme_labels =["religion", "still life", "landscape", "historical"]
 
-result = classifier(image, candidate_labels=labels)
+medium_result = classifier(image, candidate_labels=medium_labels)
+style_result = classifier(image, candidate_labels=style_labels)
+theme_result = classifier(image, candidate_labels=theme_labels)
 
-print(result)
+
+
+print("Medium:",medium_result[0]['label'], str(round(medium_result[0]['score'] * 100)) + "%")
+print("Style:",style_result[0]['label'], str(round(style_result[0]['score'] * 100)) + "%")
+print("Theme:",theme_result[0]['label'], str(round(theme_result[0]['score'] * 100)) + "%")
