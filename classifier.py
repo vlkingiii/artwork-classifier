cb3f484f383faa286069cb4bdccf34ab746f528d
vlkingiii <vernonlee.kingiii@gmail.com>
2026-04-16 23:29:16 -0700

    save classifier results to sqlite database

--- classifier.py
+++ classifier.py
@@ -1,9 +1,17 @@
 from transformers import pipeline
 from PIL import Image
+import sqlite3
+
+conn = sqlite3.connect("artworks.db")
+cursor = conn.cursor()
 
 classifier = pipeline("zero-shot-image-classification", model="openai/clip-vit-base-patch32")
 
-image = Image.open("test_art.jpg")
+title = input("Enter artwork title: ")
+artist = input("Enter artist name: ")
+
+image_path = input("Enter image path: ")
+image = Image.open(image_path)
 
 medium_labels = ["watercolor painting","gouache","fresco", "oil painting","acrylic painting", "watercolor", "pencil sketch", "digital art", "charcoal drawing"]
 style_labels = ["cubism", "neo_mannerism", "impressionism", "expressionism"]
@@ -18,3 +26,13 @@ theme_result = classifier(image, candidate_labels=theme_labels)
 print("Medium:",medium_result[0]['label'], str(round(medium_result[0]['score'] * 100)) + "%")
 print("Style:",style_result[0]['label'], str(round(style_result[0]['score'] * 100)) + "%")
 print("Theme:",theme_result[0]['label'], str(round(theme_result[0]['score'] * 100)) + "%")
+
+cursor.execute('''
+               
+    INSERT INTO artworks (title, artist, medium, style, theme)
+    VALUES (?, ?, ?, ?, ?)
+''', (title, artist, medium_result[0]['label'], style_result[0]['label'], theme_result[0]['label']))
+
+conn.commit()
+conn.close()
+print("Saved to database.")
